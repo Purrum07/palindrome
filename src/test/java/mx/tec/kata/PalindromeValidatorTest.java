652ba61ad54e27529150035b1c80a281e93a271c
Mario Márquez <48485832+Purrum07@users.noreply.github.com>
2021-04-16 17:06:41 -0600

    Step 2

--- src/test/java/mx/tec/kata/PalindromeValidatorTest.java
+++ src/test/java/mx/tec/kata/PalindromeValidatorTest.java
@@ -17,5 +17,29 @@ class PalindromeValidatorTest {
 		
 		assertEquals(expectedResult, actualResult);
 	}
+	
+	@Test
+	void Given_TwoDifferentLettersString_When_ValidatingPalindrome_Then_False() {
+		//Given
+		String string = "ma";
+		//When
+		boolean actualResult = validator.validate(string);
+		//Then
+		boolean expectedResult = false;
+		
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	void Given_TwoEqualLettersString_When_ValidatingPalindrome_Then_True() {
+		//Given
+		String string = "mm";
+		//When
+		boolean actualResult = validator.validate(string);
+		//Then
+		boolean expectedResult = true;
+		
+		assertEquals(expectedResult, actualResult);
+	}
 
 }
